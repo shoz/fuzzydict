3e00152f19a685c58c8f09b1dc1d8d4bf856bc68
Shoji Ihara <ihara@spiber.jp>
2013-09-09 16:29:24 +0900

    added a setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='fuzzydict',
+      version='0.1',
+      description='Fuzzy Matching Dictionary',
+      author='Shoji Ihara',
+      author_email='ihara@spiber.jp',
+      packages=[],
+     )
