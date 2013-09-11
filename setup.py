15d097f106ee0c38c91dc073b4211f76b17f987b
Shoji Ihara <ihara@spiber.jp>
2013-09-11 15:35:31 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from distutils.core import setup
 
-setup(name='fuzzydict',
-      version='0.1',
-      description='Fuzzy Matching Dictionary',
-      author='Shoji Ihara',
-      author_email='ihara@spiber.jp',
-      packages=[],
-     )
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+setup(
+    name='fuzzydict',
+    version='0.0.1',
+    description='FuzzyDict, an ambiguous dictionary',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        ],
+    keywords='data structure dictionary fuzzy',
+    author='Shoji Ihara',
+    author_email='shoji.ihara@gmail.com',
+    url='https://github.com/shoz/fuzzydict',
+    license='MIT'
+)
